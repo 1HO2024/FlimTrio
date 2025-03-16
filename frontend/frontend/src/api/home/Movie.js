35635ce197c472d604c9849727faf1251ca2273b
구현우 <kjklovekhw@gmail.com>
2025-03-16 17:31:12 +0900

    메인페이지 overflow 제거

--- frontend/frontend/src/api/home/Movie.js
+++ frontend/frontend/src/api/home/Movie.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 // 영화 임의로 조회하는 코드 !
+// 실제 백엔드랑 api 코드는 수정 되어야 함.
 const fetchTrendingMovies = async (accessToken) => {
   try {
     const response = await axios.get(
@@ -9,8 +10,12 @@ const fetchTrendingMovies = async (accessToken) => {
         headers: {
           Authorization: `Bearer ${accessToken}`,
         },
+        params: {
+          language: 'ko-KR',  
+        },
       }
     );
+    console.log(response);
     return response.data.results.slice(0, 10); 
   } catch (error) {
     console.error(error);
--- frontend/frontend/src/components/home/TopHome.jsx
+++ frontend/frontend/src/components/home/TopHome.jsx
@@ -1,49 +1,54 @@
 import React, { useEffect, useState } from "react";
 import fetchTrendingMovies from "../../api/home/Movie";
 import "../../style/home/TopHome.css";
-import { FaFilm, FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { FaFilm } from "react-icons/fa";
 
+// 무한루프로 코드 변경해야함 !
 const TopHome = () => {
-  // 무한루프 되게 코드 다시 짜야 됨 !
-
-  const [posters, setPosters] = useState([]);
+  const [movies, setMovies] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
   const accessToken = import.meta.env.VITE_TMDB_ACCESS_TOKEN;
 
   useEffect(() => {
-    const fetchPosters = async () => {
+    const fetchMovies = async () => {
       try {
         const moviePosters = await fetchTrendingMovies(accessToken);
-
-        const postersArray = moviePosters.map((movie) => {
-          return `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-        });
-
-        setPosters(postersArray);
+        setMovies(moviePosters);
       } catch (error) {
         console.error(error);
       }
     };
 
-    fetchPosters();
+    fetchMovies();
   }, [accessToken]);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % posters.length);
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % movies.length);
     }, 1500);
 
     return () => clearInterval(interval);
-  }, [posters.length]);
+  }, [movies.length]);
 
-  const handlePrevClick = () => {
-    setCurrentIndex(
-      (prevIndex) => (prevIndex - 1 + posters.length) % posters.length
+  const renderStars = (rating) => {
+    const roundedRating = Math.round(rating * 10) / 10;
+    const starCount = Math.round(roundedRating / 2);
+    const stars = [];
+
+    for (let i = 0; i < 5; i++) {
+      stars.push(i < starCount ? "★" : "☆");
+    }
+
+    return (
+      <div className="starRating">
+        {stars.map((star, index) => (
+          <span key={index} className="star">
+            {star}
+          </span>
+        ))}
+        <span className="ratingNumber">({roundedRating})</span>
+      </div>
     );
-  };
-
-  const handleNextClick = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % posters.length);
   };
 
   return (
@@ -52,28 +57,29 @@ const TopHome = () => {
         <FaFilm className="titleIcon" />
         <div className="titleText">오늘의 Top10 영화</div>
       </div>
-      {posters.length === 0 ? (
-        <div>포스터를 불러오는 중</div>
+      {movies.length === 0 ? (
+        <div>포스터를 불러오는 중...</div>
       ) : (
-        <div className="carouselContainer">
+        <div>
           <div
             className="posterGrid"
             style={{
               transform: `translateX(-${currentIndex * 220}px)`,
             }}
           >
-            {posters.map((poster, index) => (
-              <img key={index} src={poster} className="moviePoster" />
+            {movies.map((movie, index) => (
+              <div key={index} className="posterContainer">
+                <div className="rankingBadge">{index + 1}</div>
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  className="moviePoster"
+                  alt={`Movie ${index + 1}`}
+                />
+                <div className="movieTitle">{movie.title}</div>
+                {renderStars(movie.vote_average)}
+              </div>
             ))}
           </div>
-
-          {currentIndex > 0 && (
-            <FaArrowLeft className="arrow left" onClick={handlePrevClick} />
-          )}
-
-          {currentIndex < posters.length - 1 && (
-            <FaArrowRight className="arrow right" onClick={handleNextClick} />
-          )}
         </div>
       )}
     </div>
