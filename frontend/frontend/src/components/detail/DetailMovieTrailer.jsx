a9c0ebd6ac29f00ecfb6e7f5a991fc82e11b77f2
구현우 <kjklovekhw@gmail.com>
2025-03-17 14:08:19 +0900

    유튜브 자동재생 / 음소거 처리

--- frontend/frontend/src/components/detail/DetailMovieTrailer.jsx
+++ frontend/frontend/src/components/detail/DetailMovieTrailer.jsx
@@ -4,17 +4,25 @@ import "../../style/detail/DetailMovieTrailer.css";
 const DetailMovieTrailer = ({ movieName }) => {
   const [trailerUrl, setTrailerUrl] = useState("");
   const youtubeApikey = import.meta.env.VITE_YOUTUBE_API_KEY;
+
   useEffect(() => {
     const fetchTrailer = async () => {
-      const apiKey = youtubeApikey;
-      const response = await fetch(
-        `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${movieName}+trailer&type=video&key=${apiKey}`
-      );
-      const data = await response.json();
-      const videoId = data.items[0]?.id?.videoId;
+      try {
+        const apiKey = youtubeApikey;
+        const response = await fetch(
+          `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${movieName}+trailer&type=video&key=${apiKey}`
+        );
+        const data = await response.json();
+        const videoId = data.items[0]?.id?.videoId;
 
-      if (videoId) {
-        setTrailerUrl(`https://www.youtube.com/embed/${videoId}`);
+        if (videoId) {
+          setTrailerUrl(
+            `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`
+          );
+          // 자동 재생 + 음소거 (브라우저 정책 회피)
+        }
+      } catch (error) {
+        console.error("유튜브 API 오류:", error);
       }
     };
 
@@ -30,6 +38,7 @@ const DetailMovieTrailer = ({ movieName }) => {
         height="315"
         src={trailerUrl}
         frameBorder="0"
+        allow="autoplay"
         allowFullScreen
         title="Movie Trailer"
       ></iframe>
